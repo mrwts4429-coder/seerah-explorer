a60a69135adfe3eb89ce8b177604d893bba72b8b
Marwa Sayed Hassan <mrwts4429@gmail.com>
2026-09-14 23:08:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/seerah-explorer/', // <-- أضيفي هذا السطر باسم الريبو الخاصة بك
 })
